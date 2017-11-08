02edcb521e417e527f6ed9da459f512a465a4d45
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-11-08 16:26:18 -0500

    Use common conf.py via docs-conf project
    Change-Id: I371be992b35ad2fa5d06f739938b994ac0602761
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ pinfo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Project info file."""
+
+__project__ = 'lf-releng-docs'
+__author__ = 'Linux Foundation Releng'
+__copyright__ = '2017, The Linux Foundation'
+__version__ = 'master'
